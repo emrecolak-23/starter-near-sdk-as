19045ea196cf27cf0efa1ac1428455b5139f0e70
Emre Çolak <colakkemre@gmail.com>
2022-04-11 22:57:17 +0300

    task 1 completed

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,8 +1,8 @@
-import { storage, Context } from "near-sdk-as"
+import { storage, Context } from "near-sdk-as";
 
 // return the string 'hello world'
 export function helloWorld(): string {
-  return 'hello world'
+  return "hello emre";
 }
 
 // read the given key from account (contract) storage
